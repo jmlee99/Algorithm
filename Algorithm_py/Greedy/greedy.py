82a534b04bdbcfb9749ce337f00662c132bbdfe2
jjam <ljm9017@naver.com>
2023-05-17 19:57:43 +0900

    commit on 5/17

--- Algorithm_py/Greedy/greedy.py
+++ Algorithm_py/Greedy/greedy.py
@@ -60,7 +60,7 @@ while True:
 
 print(result)
 
-# 답안 k를 반복문에 시간복잡도 낮춤
+# 답안 k를 반복문에 시간복잡도 더 낮춤
 
 # N, M, K를 공백으로 구분하여 입력 받기
 n, m, k = map(int, input().split())
---
+++ Algorithm_py/Greedy/greedy_01.py
@@ -0,0 +1,25 @@
+# 숫자 카드 게임
+# 내 답안
+n, m = map(int, input().split())
+
+min_value = []
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value.append(min(data))
+
+print(max(min_value))
+
+# 정답안
+# N,M을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+
+result = 0
+# 한 줄씩 입력 받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = min(data)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+
+print(result)
